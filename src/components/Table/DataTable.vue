aa000d67a79668eb2280926373e4e576680bd307
Kadir Ali <kadirali552@gmail.com>
2021-10-12 14:56:36 +0200

    Updated Data Table commponent

--- src/components/Table/DataTable.vue
+++ src/components/Table/DataTable.vue
@@ -1,33 +1,23 @@
 <template>
   <v-container>
     <v-card>
-        <h1 v-if="showTitle">{{title}}</h1>
-      
-      <v-data-table  :headers="headers" :items="items" >
-     <template v-slot:item.actions="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
-    </template>
+      <h1 v-if="showTitle">{{ title }}</h1>
+
+      <v-data-table :headers="headers" :items="items">
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)">
+            mdi-delete
+          </v-icon>
+        </template>
       </v-data-table>
     </v-card>
   </v-container>
 </template>
 
-
 <script>
-
-
 export default {
   data: () => ({
     //   tableHeaders: [],
@@ -51,21 +41,19 @@ export default {
       default: "",
     },
     showTitle: {
-        type: Boolean,
-        // required: false,
-        // default: ""
-    
-    }
+      type: Boolean,
+      // required: false,
+      // default: ""
+    },
   },
   // mounted() {
   //     this.tableItems = this.items
   //     this.tableHeaders = this.headers
 
   //   axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => (this.posts = response.data))
-      
+
   // }
 };
 </script>
 
-<style>
-</style>
+<style></style>
