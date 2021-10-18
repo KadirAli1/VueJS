2a7d4424d019860c7ba072b50bae023fc01db301
Kadir Ali <50846277+KadirAli1@users.noreply.github.com>
2021-10-18 12:18:00 +0200

    added new button in data table component for adding new Posts

--- src/components/Table/DataTable.vue
+++ src/components/Table/DataTable.vue
@@ -1,8 +1,88 @@
 <template>
   <v-container>
+       <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+          class="add-posts"
+        >
+          Add post
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">New Post</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Title "
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Body"
+                  hint="example of helper text only on focus"
+                ></v-text-field>
+              </v-col>
+            
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="User Id"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
     <v-card>
+  
       <h1 v-if="showTitle">{{ title }}</h1>
-
+   
       <v-data-table :headers="headers" :items="items">
         <template v-if="showIcon" v-slot:[`item.actions`]="{ item }">
           <!-- <router-link to="`user/${item.id}`" > -->
@@ -25,8 +105,7 @@
 <script>
 export default {
   data: () => ({
-    //   tableHeaders: [],
-    //   tableItems: [],
+   dialog: false,
   }),
   methods: {
     goToUser(userId) {
@@ -69,7 +148,8 @@ export default {
 </script>
 
 <style>
-.mr-2 {
-  text-decoration: none;
+.add-posts{
+  margin: 10px;
+  margin-bottom: 10px;
 }
 </style>
